fe4a8089631f4f94c70bf17f758b7ffe664a38b9
Hernan Fernandez <hernanjx@gmail.com>
2025-04-22 23:14:18 -0300

    Update products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@
 class Product < ApplicationRecord
   include Evdatumable
 
+  after_create_commit :create_evdatum
+
   validates :name, presence: true
   validates :sku, presence: true, format: { with: /\A[a-zA-Z0-9-]+\z/ }, length: { maximum: 20 }, uniqueness: true
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "must have a name" do
+    product = Product.new
+
+    assert_not product.valid?
+    assert_includes product.errors[:name], "can't be blank"
+  end
+
+  test "must have a sku" do
+    product = Product.new(name: "Test Product")
+
+    assert_not product.valid?
+    assert_includes product.errors[:sku], "can't be blank"
+  end
+
+  test "must have a valid sku" do
+    product = Product.new(name: "Test Product", sku: "AB-C123")
+
+    assert product.valid?
+  end
+
+  test "must have a valid sku of max length 20 chars" do
+    product = Product.new(name: "Test Product", sku: "ABC12345678901234567")
+
+    assert product.valid?
+  end
+
+  test "must fail if the sku is too long" do
+    product = Product.new(name: "Test Product", sku: "ABC12345678901234567890")
+
+    assert_not product.valid?
+    assert_includes product.errors[:sku], "is too long (maximum is 20 characters)"
+  end
+
+  test "must normalize the sku to uppercase" do
+    sku = "abc123"
+    product = Product.new(name: "Test Product", sku: sku)
+
+    assert product.valid?
+    assert_equal sku.upcase, product.sku
+  end
 end
